df9354f1dad412d9615c4c68e68a1d7851d919e8
Benjamin Faught <benjaminfaught007@gmail.com>
2017-04-06 11:43:14 -0500

    adds completed exercise files

---
+++ 01_getting-started/02_numeral-systems/03_hexadecimal/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Printf("%d - %b - %x \n", 55, 55, 55)
+	fmt.Printf("%d - %b - %#x \n", 55, 55, 55)
+	fmt.Printf("%d - %b - %#X \n", 55, 55, 55)
+	fmt.Printf("%d \t %b \t %#x \n", 55, 55, 55)
+
+}
